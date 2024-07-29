1a475e5c5bd36fdf29b6de004c4b527571180901
jounad tourvieille <jounad.tourvieille@axa.fr>
2024-07-29 14:22:33 +0200

    feat(module1): solution module1

---
+++ MyApi/MyApi.WebApi.Tests/Features/CalculatorSteps.cs
@@ -0,0 +1,31 @@
+﻿using TechTalk.SpecFlow;
+
+namespace MyApi.WebApi.Tests.Features;
+
+[Binding]
+internal class CalculatorSteps
+{
+    [Given("the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int firstNumber)
+    {
+
+    }
+
+    [Given("the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int secondNumber)
+    {
+
+    }
+
+    [When(@"the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(int expectedResult)
+    {
+
+    }
+}
